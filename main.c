b573bb666982ad7721a204afe499faec8def15d2
unknown <classicmanguten@gmail.com>
2024-04-27 03:03:55 -0700

    Comitting c program files

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int Old meter reading =100;
+    int New meter reading =150;
+     printf("Old meter, %d")
+
+
+    return 0;
+}
---
+++ pro/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int Salary = 5000;
+  double Rate = 200.25;
+  int Bonus = 250;
+  int Age = 15;
+  char Grade = 'F';
+  float Discount = 15.5;
+  short Marks = 55;
+  long Quantity =100;
+  double Average = 25.55;
+
+  printf("The Salary is %d" Salary);
+  printf("The Rate is %LF"Rate);
+  printf("The Bonus is %d" Bonus);
+  printf("The Age is %d"Age);
+  printf("The Grade is %c"Grade);
+  printf("The Discount is %f"Discount);
+  printf("The Marks is %hu"Marks);
+  printf("The Quantity is %Id" Quantity);
+  printf("The Average is %lf" Average);
+
+    return 0;
+}
